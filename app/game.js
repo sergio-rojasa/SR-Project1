67b4c5bfd70a0d10e31aa08da39ab9616d85bba4
Sergio Rojas <serg.rojas@tutamail.com>
2017-06-09 19:09:34 -0700

    refactor

--- app/game.js
+++ app/game.js
@@ -1,56 +1,79 @@
 var Game = function() {
   this.power = false;
   this.strictMode = false;
-  this.currentStep = 0;
+  this.signalNumber = 0;
+  this.speed = 900;
   this.currentPlayer = null;
+  this.players = {};
   this.audio = {
     'green': new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
     'red':  new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
     'yellow': new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
     'blue':  new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
   };
-  this.players = {};
-  this.speed = 900;
 };
 
 Game.prototype.togglePower = function() {
-  this.power = !this.power;
+    this.power = !this.power;
 };
 Game.prototype.getPower = function() {
-  return this.power;
-};
-Game.prototype.toggleStrictMode = function() {
-  this.strictMode = !this.strictMode;
-};
-Game.prototype.getStrictMode = function() {
-  return this.strictMode;
+    return this.power;
 };
 
-Game.prototype.addCurrentStep = function() {
-  this.currentStep++;
+Game.prototype.toggleStrictMode = function() {
+    this.strictMode = !this.strictMode;
 };
-Game.prototype.clearCurrentStep = function() {
-  this.currentStep = 0;
+Game.prototype.getStrictMode = function() {
+    return this.strictMode;
 };
-Game.prototype.getCurrentStep = function() {
-  return this.currentStep;
+
+Game.prototype.clearSignalNumber = function() {
+    this.signalNumber = 0;
 };
+Game.prototype.addOneSignal = function() {
+    this.signalNumber++;
+};
+Game.prototype.getCurrentSignalNumber = function() {
+    return this.signalNumber;
+};
+
+Game.prototype.setCurrentPlayer = function(player) {
+        this.currentPlayer = player;
+};
+Game.prototype.getCurrentPlayer = function() {
+    return this.currentPlayer;
+};
+
+Game.prototype.addPlayer = function(name) {
+    this.players[name] = new Player();
+};
+
+Game.prototype.playTone = function(signal) {
+    this.audio[signal].play();
+}
+Game.prototype.lightUpLens = function(signal) {
+    var colorLens = document.getElementById(signal);
+
+    colorLens.classList.add('click');
+    setTimeout(function() {
+        colorLens.classList.remove('click');
+    }, this.speed / 2);
+};
+
+
 Game.prototype.displayCurrentStep = function() {
     var digits = document.getElementById('digits');
 
     digits.innerHTML = this.getCurrentStep();
 };
+Game.prototype.clearDisplayScreen = function() {
+    if(!this.getPower()) {
+      var digits = document.getElementById('digits');
 
-Game.prototype.getCurrentPlayer = function() {
-  return this.currentPlayer;
+      digits.innerHTML = "";
+    }
+
 }
-Game.prototype.setCurrentPlayer = function(player) {
-  this.currentPlayer = player;
-};
-
-Game.prototype.playAudio = function(pad) {
-  this.audio[pad].play();
-};
 
 Game.prototype.toggleLed = function(led) {
   var led = document.getElementById(led);
@@ -72,19 +95,9 @@ Game.prototype.toggleStrictModeLed = function() {
     strict.classList.remove('click');
   }
 }
-Game.prototype.lightUp = function(pad) {
-  var pad = document.getElementById(pad);
 
-  pad.classList.add('click');
 
- setTimeout(function() {
-    pad.classList.remove('click');
-  }, this.speed / 2);
-};
 
-Game.prototype.addPlayer = function(name) {
-  this.players[name] = new Player();
-}
 
 Game.prototype.init = function() {
   this.addPlayer('human');
